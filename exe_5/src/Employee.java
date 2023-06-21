21af50bafa2cacec147b756d575ee8f1980c7005
BrianaNzivu <bjnzivu@gmail.com>
2023-06-22 01:07:13 +0300

    Figuring shit

---
+++ exe_5/src/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+    private String name;
+    private int number;
+
+    public void inputData(){
+        this.name = name;
+        this.number = number;
+    }
+
+    public void display(){
+        System.out.print("Student info" + name + " " + number);
+    }
+}
---
+++ exe_5/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        Scanner s = new Scanner(System.in);
+        System.out.println(e1.);
+
+        e1.inputData();
+        e1.display();
+    }
+}
